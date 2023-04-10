5facd5bc253874d9528e29c0676dc25672f9568b
Alexandr Vinogradov <izum709@.com>
2023-04-10 21:40:28 +0300

    Add find  max number

---
+++ Exemple_4/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+//2, 3, 7 -> 7
+//44 5 78 -> 78
+//22 3 9 -> 22
+
+Console.WriteLine("Введи число ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введи еще число ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Ну и еще одно ");
+int c = int.Parse(Console.ReadLine());
+int max = a;
+if (max < b) max = b;
+if (max < c) max = c;
+Console.WriteLine($"{max} - это максимальное число!");
